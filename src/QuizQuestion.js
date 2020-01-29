f6510d3c8a509839755e7f06ba78803e53e240da
Havvaus5 <havvauslu5@gmail.com>
2020-01-29 22:39:36 +0300

    section 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
-    render() {
+  handleClick(buttonText){
+    if(buttonText === this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler()
+    }
+  }  
+  
+  render() {
         return (
             <main>
             <section>
@@ -10,8 +16,12 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
               <ul>
-                  <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]}></QuizQuestionButton>
-                {/* <li>{this.props.quiz_question.answer_options[0]}</li> */}
+                {
+                  this.props.quiz_question.answer_options.map((answer_option, index) => {
+                    return  <QuizQuestionButton key={index} button_text = {answer_option}
+                    clickHandler ={this.handleClick.bind(this)}></QuizQuestionButton>
+                  })
+                }
               </ul>
             </section>
           </main>
